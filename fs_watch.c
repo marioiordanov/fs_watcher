a6886325de3109e798d8d48e9d565a1dd287719d
marioiordanov <mio@shardlabs.io>
2026-02-13 12:34:11 +0200

    descriptive messages

--- fs_watch.c
+++ fs_watch.c
@@ -278,8 +278,10 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    printf("Start watching for changes in %s\n", dir_path);
+
     wait_for_ctrl_c();
-    printf("Cleanning up....");
+    printf("Cleanning up....\n");
 
     FSEventStreamInvalidate(streamRef);
     FSEventStreamStop(streamRef);
