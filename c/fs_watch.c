4611a1a821cde4f7f9d2e4b81e58a96569fb99d6
marioiordanov <mio@shardlabs.io>
2026-03-17 16:35:04 +0200

    use latency from argument list

--- c/fs_watch.c
+++ c/fs_watch.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "watcher.h"
 
 int main(int argc, char** argv) {
-    if (argc != 2) {
+    if (argc != 3) {
         fprintf(stderr, "usage: fs_watch <dir>\n");
         return 1;
     }
 
-    const double latency = 0.5;
+    double latency = atof(argv[2]);
+
     return run_watcher(argv[1], latency) ? 0 : 1;
 }
