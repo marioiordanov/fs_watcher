8a93d9f44b27794f59d9ef78464a34546d16aff7
marioiordanov <mio@shardlabs.io>
2026-04-30 23:58:46 +0300

    correct sliding window algorithm, with correct order of elements insertion

--- c/fs_watch.c
+++ c/fs_watch.c
@@ -16,6 +16,10 @@ int main(int argc, char** argv) {
 
     const size_t window_len = 3;
     int* window_ptrs[window_len] = {0};
+    // int window[window_len] = {0};
+    // for (size_t i = 0; i < window_len; i++) {
+    //     window_ptrs[i] = &window[i];
+    // }
     int arr_data[] = {7,3,8,1,2, 4, 19, 6, 5, 11, 10};
 
     size_t arr_len = sizeof(arr_data) / sizeof(arr_data[0]);
@@ -26,33 +30,30 @@ int main(int argc, char** argv) {
         // load as much to fill the sliding window
         // `consumed` shows how much elements are being used, use it to fill the rest
 
-        size_t load_up_to_index = i + elements_to_load > arr_len ? arr_len : i + elements_to_load;
-        printf("load to index %lu\n", load_up_to_index );
+        printf("elements to load %lu\n", elements_to_load );
 
         size_t k = i;
-        for (; k < load_up_to_index; k++) {
-            window_ptrs[ k % window_len ] = &arr_data[k];
+        for (size_t t = i; t < i + elements_to_load; t++) {
+            if (t < arr_len) {
+                window_ptrs[ t % window_len ] = &arr_data[t];
+                k++;
+            }else {
+                window_ptrs[ t % window_len ] = NULL;
+            }
         }
 
         printf("last loaded index %lu\n", k - 1);
         printf("order window from oldest to newest: \n");
         for (size_t t = 0; t < window_len; t++) {
-            printf("%d ", *window_ptrs[ (k + t) % window_len ]);
+            if (window_ptrs[(k+t) % window_len] != NULL) {
+                printf("%d ", *window_ptrs[ (k + t) % window_len ]);
+            }
         }
         printf("\n");
-
-        // for (size_t t = 0; t < window_len; t++) {
-        //     printf("%d ", *window_ptrs[t]);
-        // }
-        // printf("\n");
-
-        // operate on window
 
         scanf("%d", &consumed);
         i+=elements_to_load;
         elements_to_load = consumed;
-
-
     }
 
     return 0;
